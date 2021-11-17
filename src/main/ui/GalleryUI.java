38e733a4af5e297893a8e758f7f4467447096dd7
katielouie <katie.jt.louie@gmail.com>
2021-11-16 16:02:56 -0800

    Clean up code and add method specifiers

--- src/main/ui/GalleryUI.java
+++ src/main/ui/GalleryUI.java
@@ -15,9 +15,8 @@ public class GalleryUI extends JFrame {
     private static final int HEIGHT = 800;
     private static final String JSON_GALLERY = "./data/gallery.json";
 
-    private JDesktopPane desktop;
+    private final JDesktopPane desktop;
     JPanel listPanel;
-    JPanel viewPanel;
 
     private DefaultListModel<ArtPiece> listModel;
     private JList<ArtPiece> list;
@@ -25,9 +24,8 @@ public class GalleryUI extends JFrame {
     private JLabel picture;
 
 
-    //Constructor to make the window
+    // EFFECTS: Constructor to make the window
     public GalleryUI() {
-        Gallery gallery = new Gallery();
 
         desktop = new JDesktopPane();
         setContentPane(desktop);
@@ -42,7 +40,7 @@ public class GalleryUI extends JFrame {
         setVisible(true);
     }
 
-    //Split the screen to add save, load, list, and edit menus
+    // EFFECTS: Split the screen to add save, load, list, and edit menus
     public void addGalleryPanel() {
         listPanel = new JPanel();
         listPanel.setPreferredSize(new Dimension(250,800));
@@ -54,7 +52,7 @@ public class GalleryUI extends JFrame {
         addEditButtons();
     }
 
-    // Shows art piece, title, medium, and subject
+    // EFFECTS: Shows art piece, title, medium, and subject
     public void addViewPanel() {
         //viewPanel = new JPanel();
         picture = new JLabel();
@@ -63,7 +61,7 @@ public class GalleryUI extends JFrame {
         desktop.add(scrollPane, BorderLayout.CENTER);
     }
 
-    // Add save/Load Buttons
+    // EFFECTS: Add save/Load Buttons
     public void addDataButtons() {
         JPanel btnPnl = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
@@ -79,25 +77,27 @@ public class GalleryUI extends JFrame {
         listPanel.add(btnPnl, BorderLayout.NORTH);
     }
 
+    // EFFECTS: Add List to ScrollPane
     public void addList() {
         listModel = new DefaultListModel<>();
         list = new JList<>(listModel);
         list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         list.setSelectedIndex(0);
         list.setVisibleRowCount(5);
-        list.addListSelectionListener(e -> update());
+        list.addListSelectionListener(e -> updatePicture());
         listScrollPane = new JScrollPane(list);
         listPanel.add(listScrollPane);
     }
 
-    protected void update() {
+    // EFFECTS: Updates picture to selected value
+    protected void updatePicture() {
         if (!list.isSelectionEmpty()) {
             ImageIcon icon = new ImageIcon(list.getSelectedValue().getPath());
             picture.setIcon(icon);
         }
     }
 
-    // Add add/remove Buttons
+    // EFFECTS: Add buttons to add/remove
     public void addEditButtons() {
         JPanel btnPnl = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JButton addButton = new JButton("Add");
@@ -110,6 +110,7 @@ public class GalleryUI extends JFrame {
         listPanel.add(btnPnl, BorderLayout.SOUTH);
     }
 
+    // EFFECTS: Pop-Up Menu for inputting data for new artPieces
     public void addMenu() {
         JInternalFrame frame = new JInternalFrame("Add", false, true);
         JPanel panel = new JPanel();
@@ -141,6 +142,8 @@ public class GalleryUI extends JFrame {
         frame.show();
     }
 
+    // REQUIRES: Text fields for title, subject, medium, and path
+    // EFFECTS: Method for getting the art piece and adding it to the list
     public void addArtPiece(TextField titleField, TextField subjectField, TextField mediumField, TextField pathField) {
         String title = titleField.getText();
         String subject = subjectField.getText();
@@ -150,6 +153,7 @@ public class GalleryUI extends JFrame {
         listModel.addElement(artPiece);
     }
 
+    // EFFECTS: Removes artPiece from List
     public void removeArtPiece() {
         if (!list.isSelectionEmpty()) {
             listModel.remove(list.getSelectedIndex());
@@ -158,6 +162,7 @@ public class GalleryUI extends JFrame {
         list.updateUI();
     }
 
+    // EFFECTS: Saves Gallery to file
     public void save() {
         Gallery gallery = new Gallery();
 
